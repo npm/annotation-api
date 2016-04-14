7239fdb4a3d004c32732acbcd8ca7fdc4e9348c8
Benjamin Coe <ben@npmjs.com>
2016-04-13 18:25:41 -0700

    feat(webhook): built out handler for fetching annotation data from external integrations

--- test/client-manager.js
+++ test/client-manager.js
@@ -17,6 +17,7 @@ describe('ClientManager', function () {
       var clients = nock('http://0.0.0.0:8084')
         .get('/client')
         .reply(200, fs.readFileSync('./test/fixtures/clients.json', 'utf-8'))
+
       var clientManager = new ClientManager()
       clientManager.load(function (err) {
         expect(err).to.equal(undefined)
@@ -41,7 +42,7 @@ describe('ClientManager', function () {
   })
 
   describe('annotationForPageLoad', function () {
-    var annotation1 = {
+    var annotation = {
       id: 'abc-123-abc',
       status: 'green',
       'status-message': 'module scanned',
@@ -49,21 +50,66 @@ describe('ClientManager', function () {
       'external-link': 'http://example.com/foo-package/audit',
       'external-link-text': 'view details'
     }
-    var annotations = [
-      annotation1
-    ]
+    var clients = JSON.parse(fs.readFileSync('./test/fixtures/clients.json', 'utf-8'))
 
     beforeEach(function () {
+      delete annotation.fingerprint
       client.flushdb()
     })
 
-    it('returns annotation from redis if it exists in the database', function (done) {
+    it('returns annotation from cache it is populated', function (done) {
       var clientManager = new ClientManager()
-      clientManager.cacheAnnotations('foo-pkg', annotations, function () {
-        clientManager.annotationsForPageLoad('foo-pkg', function (annotations) {
+      var pkgName = 'foo-pkg'
+
+      clientManager.cacheAnnotations(pkgName, [annotation], function () {
+        clientManager.annotationsForPageLoad(pkgName, function (annotations) {
           var annotation = annotations[0]
           annotation.id.should.equal('abc-123-abc')
           annotation.fingerprint.should.match(/[a-z0-9]{64}/)
+          return done()
+        })
+      })
+    })
+
+    it('requests annotations from an external API if cache is not populated', function (done) {
+      var pkgName = 'foo-pkg'
+      var payload = JSON.stringify({
+        event: 'package:page-load',
+        package: pkgName,
+        sender: {
+          email: clients[0].tokens[0].user_email
+        }
+      })
+      var clientManager = new ClientManager()
+      var signature = clientManager.sign(payload, clients[0])
+      var getClients = nock('http://0.0.0.0:8084')
+        .get('/client')
+        .reply(200, [clients[0]])
+      // mock requesting a payload from an external
+      // service. the webhook endpoint is stored on
+      // the OAuth client.
+      var getAnnotation = nock('http://www.example.com', {
+        reqheaders: {
+          'npm-signature': signature,
+          'content-type': 'application/json'
+        }
+      })
+        .post('/fetch/data', payload)
+        .reply(200, annotation)
+
+      clientManager.load(function (err) {
+        expect(err).to.equal(undefined)
+        clientManager.annotationsForPageLoad(pkgName, function (annotations) {
+          expect(err).to.equal(undefined)
+
+          var annotation = annotations[0]
+          // the client-id gets written to the
+          // annotation as a unique identifier.
+          annotation.id.should.equal(clients[0].client_id)
+          annotation.fingerprint.should.match(/[a-z0-9]{64}/)
+
+          getClients.done()
+          getAnnotation.done()
           return done()
         })
       })
